b55d38f3015d9a227abf44b571adf6fac7c5c509
TiagoKaimbra <Tiago08santos08@gmail.com>
2025-02-24 15:31:24 -0300

    Handler de validação do email e senha

--- src/main/java/dev/wakandaacademy/produdoro/handler/RestResponseEntityExceptionHandler.java
+++ src/main/java/dev/wakandaacademy/produdoro/handler/RestResponseEntityExceptionHandler.java
@@ -1,8 +1,14 @@
 package dev.wakandaacademy.produdoro.handler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import lombok.extern.log4j.Log4j2;
@@ -25,5 +31,17 @@ public class RestResponseEntityExceptionHandler {
 						.message("POR FAVOR INFORME AO ADM DO SISTEMA!")
 						.description("INTERNAL SERVER ERROR!")
 						.build());
+	}
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handlerValidationExceptions(MethodArgumentNotValidException exception){
+		Map<String, String> errors = new HashMap<>();
+		exception.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		return errors;
 	}
 }
